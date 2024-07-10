2596ca6186856f7cce66efc9ee330ea548d4d00c
Sofia Rodriguez del Busto <rdbsofi@gmail.com>
2024-07-10 12:11:48 -0300

    Agrego metodo para dar descripciones de los ataques

--- Batalla.cs
+++ Batalla.cs
@@ -4,6 +4,7 @@ namespace Juego
     {   
         public Personaje GeneradorDeBatalla(Personaje personaje1, Personaje personaje2)
         {
+            Batalla batalla = new Batalla();
             Console.WriteLine($"Se encuentran {personaje1.Nombre} y {personaje2.Nombre} en la arena");
             Console.WriteLine("Es el momento de enfretarse y solo uno de ellos seguirá en competencia");
             Personaje aux;
@@ -20,6 +21,7 @@ namespace Juego
                 {
                     atacante.MejorAtaque = danio;
                 }
+                batalla.ElegirJugadaDeAtaque(danio);
                 Console.WriteLine($"El daño provocado por el atacante es {danio}");
                 Console.WriteLine($"La salud de los jugadores luego del ataque: ");
                 Console.WriteLine($"Salud {atacante.Nombre}: {atacante.Salud}");
@@ -42,7 +44,46 @@ namespace Juego
                 return personaje2;
             }
         }
-        
+
+
+        public void ElegirJugadaDeAtaque(int danio)
+        {
+            string[] AtaquesFuertes = {
+                "Dispara su flecha y acierta en el pecho de su oponente generando un grave sangrado.",
+                "Dispara su flecha y ésta se clava directamente en el rostro del otro tributo.",
+                "Aprovecha la distracción de su oponente y logra clavar su cuchillo en el abdomen del mismo.",
+                "Toma su lanza y la arroja con gran fuerza y velocidad hacia su oponente. Logra acertarle a su cuerpo.",
+                "Se encuentra con un fuego encendido, calienta su arma y la arroja generando grandes quemaduras y heridas."
+            };
+
+            string[] AtaquesMedios = {
+                "Dispara su flecha y ésta golpea la pierna del oponente dificultando su movimiento.",
+                "Agarra un mazo y golpea a su oponente por la espalda causándole una grave caida.",
+                "Toma las piedras que se encuentran a su alrededor y logra disminuir a su oponente.",
+                "Lanza su cuerpo hacia su oponente y logra golpearlo directamente con sus puños.",
+                "Logra levantar a su oponente y lo arroja directamente contra un árbol."
+            };
+
+            string[] AtaquesDebiles = {
+                "Logra alcanzar unas piedras pequeñas y las apunta contra su oponente provocandole poco daño y distrayendolo.",
+                "Ataque directamente a su oponente pero solo alcanza a darle una patada y cae contra el suelo.",
+                "Logra inmovilizar una de las extremidades del oponente con una cuerda.",
+                "Con palo logra golpear y alejar a su oponente unos metros.",
+                "Lanza su flecha contra el oponente pero solo logra rozar uno de sus brazos."
+            };
+            var semilla = Environment.TickCount;
+            var random = new Random(semilla); 
+            if(danio > 40)
+            {
+                Console.WriteLine(AtaquesFuertes[random.Next(0,5)]);
+            }else if (danio > 15)
+            {
+                Console.WriteLine(AtaquesMedios[random.Next(0,5)]);
+            }else
+            {
+                Console.WriteLine(AtaquesDebiles[random.Next(0,5)]);
+            }
+        }
     }
     
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Text.Json;
 using Juego;
 
+
 interfazGrafica interfaz = new interfazGrafica();
 
 
